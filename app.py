4dc2be54c955f459c5433919ec51b9dc87539fd5
James Prentice <jamesprenticez@gmail.com>
2022-08-07 21:44:07 +1200

    Sessions and Logout

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
-from flask import Flask, app, request, redirect, url_for, render_template
+from flask import Flask, app, session, request, redirect, url_for, render_template
+from datetime import timedelta
 
 app = Flask(__name__)
-app.config["SEND_FILE_MAX_AGE_DEFAULT"] = -1
+app.jinja_env.cache = {}
+app.secret_key = "I68#nhKm6Z&@ZPB6!WxqA7NPE"
+app.permanent_session_lifetime = timedelta(hours=12)
 
 @app.route("/")
 def home():
   return render_template("index.html", content=["BTC", "ETH", "XRP"])
 
-@app.route("/dashboard")
+@app.route("/dashboard/")
 def dashboard():
   return render_template("dashboard.html", content=["BTC", "ETH", "XRP"])
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login/", methods=["POST", "GET"])
 def login():
   if request.method == "POST":
+    session.permanent = True
     user = request.form["username"]
-    return redirect(url_for("user", user=user))
+    session["user"] = user
+    return redirect(url_for("user"))
   else:
+    if "user" in session:
+      return redirect(url_for("user")) 
+
     return render_template("login.html")
 
-@app.route("/user/<user>")
-def user(user):
-  return f"<h1>{user}</h1>"
+@app.route("/logout/")
+def logout():
+  session.pop("user", None)
+  return redirect(url_for("login"))
+
+@app.route("/user/")
+def user():
+  if "user" in session:
+    user = session["user"]
+    return f"<h1>{user}</h1>"
+  else:
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
   app.run(debug=True)
