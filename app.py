08086a73b6f3cfbc54f13b30b427619fab642fac
James Prentice <jamesprenticez@gmail.com>
2022-08-07 23:05:34 +1200

    Flashing messages and creating mock routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, app, session, request, redirect, url_for, render_template
+from flask import Flask, app, session, request, redirect, url_for, render_template, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -7,12 +7,44 @@ app.secret_key = "I68#nhKm6Z&@ZPB6!WxqA7NPE"
 app.permanent_session_lifetime = timedelta(hours=12)
 
 @app.route("/")
+def index():
+  if "user" in session:
+    user = session["user"]
+    return redirect(url_for("dashboard"))
+  else:
+    return render_template("index.html")
+
+@app.route("/home/")
 def home():
-  return render_template("index.html", content=["BTC", "ETH", "XRP"])
+  user = session["user"]
+  return render_template("home.html", user=user)
 
 @app.route("/dashboard/")
 def dashboard():
-  return render_template("dashboard.html", content=["BTC", "ETH", "XRP"])
+  user = session["user"]
+  return render_template("dashboard.html", user=user)
+
+@app.route("/trades/")
+def trades():
+  user = session["user"]
+  return render_template("trades.html", user=user)
+
+@app.route("/account/")
+def account():
+  if "user" in session:
+    user = session["user"]
+    return render_template("account.html", user=user)
+  else:
+    flash("You are not logged in!")
+    return redirect(url_for("login"))
+
+@app.route("/settings/")
+def settings():
+  if "user" in session:
+    user = session["user"]
+    return render_template("settings.html", user=user)
+  else:
+    return redirect(url_for("login"))
 
 @app.route("/login/", methods=["POST", "GET"])
 def login():
@@ -20,25 +52,21 @@ def login():
     session.permanent = True
     user = request.form["username"]
     session["user"] = user
-    return redirect(url_for("user"))
+    flash("Login Succesfull!", "info")
+    return redirect(url_for("account"))
   else:
     if "user" in session:
-      return redirect(url_for("user")) 
+      flash("Already Logged In!", "info")
+      return redirect(url_for("account")) 
 
     return render_template("login.html")
 
 @app.route("/logout/")
 def logout():
+  user = session["user"]
+  flash(f"User {user} has been logged out!", "info")
   session.pop("user", None)
   return redirect(url_for("login"))
-
-@app.route("/user/")
-def user():
-  if "user" in session:
-    user = session["user"]
-    return f"<h1>{user}</h1>"
-  else:
-    return redirect(url_for("login"))
 
 if __name__ == "__main__":
   app.run(debug=True)
